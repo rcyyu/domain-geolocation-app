46fc8049fc032e3184dcb6613ff2fc09ca5ebeb7
rcyyu <ronald.c.yu@gmail.com>
2020-03-01 15:53:09 -0500

    Removed user code

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
+// Register at ipstack.com to get a free API key and enter it here
 const apiKey = '298877b144bc9c3f4165ea0fe436f486';
 
 // Get the index file
@@ -18,24 +19,20 @@ app.get('/', function (req, res) {
 // Load geolocated page from index
 app.get('/geolocate?', function (req, res) {
     let domain = req.query.domain;
-    console.log(domain);
-
-    // Get user IP
-    var ip = (req.headers['x-forwarded-for'] || '').split(',').pop() || 
-         req.connection.remoteAddress || 
-         req.socket.remoteAddress || 
-         req.connection.socket.remoteAddress
-    console.log('user:', ip);
 
     // Get domain IP
     dns.lookup(domain, function onLookup(err, addresses, family) {
+        // Address(es) is the IP address of the domain
         let url = `http://api.ipstack.com/${addresses}?access_key=${apiKey}`
         if (err) {
+            // If DNS lookup errored
             res.render('geolocated', { domain: null, location: null, error: 'Error, please enter a valid domain'});
         } else {
+            // Geolocate IP address using ipstack API
             request(url, function(err, response, body) {
                 let location = JSON.parse(body);
                 if (err) {
+                    // If ipstack API errored
                     res.render('geolocated', { domain: domain, location: null, error: 'Error, please try again' });
                 } else {
                     res.render('geolocated', { domain: domain, location: location, error: null });
