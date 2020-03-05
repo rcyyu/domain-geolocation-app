a57b5229e37083faef80bb9d8148dcd9f464e067
rcyyu <ronald.c.yu@gmail.com>
2020-03-05 12:06:18 -0500

    Fix user ip lookup

--- server.js
+++ server.js
@@ -11,12 +11,6 @@ app.set('view engine', 'ejs')
 // Register at ipstack.com to get a free API key and enter it here
 const apiKey = '298877b144bc9c3f4165ea0fe436f486';
 
-// Get user's local IP
-var user_ip = (req.headers['x-forwarded-for'] || '').split(',').pop() || 
-         req.connection.remoteAddress || 
-         req.socket.remoteAddress || 
-         req.connection.socket.remoteAddress;
-
 // Get the index file
 app.get('/', function (req, res) {
     res.render('index');
@@ -25,6 +19,12 @@ app.get('/', function (req, res) {
 // Load geolocated page from index
 app.get('/geolocate?', function (req, res) {
     let domain = req.query.domain;
+
+    // Get user's local IP
+    var user_ip = (req.headers['x-forwarded-for'] || '').split(',').pop() || 
+        req.connection.remoteAddress || 
+        req.socket.remoteAddress || 
+        req.connection.socket.remoteAddress;
 
     // Get domain IP
     dns.lookup(domain, function onLookup(err, domain_ip, family) {
